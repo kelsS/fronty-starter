e597548cc32750029f2f87114df0cdebdf5fc505
kelsS <kswanson426@gmail.com>
2018-08-05 22:14:29 -0400

    add mix it up test for cod program listinh

--- src/js/global.js
+++ src/js/global.js
@@ -6,3 +6,6 @@ console.log("Global JS example");
 const testVar = "Bable was a success";
 
 console.log(testVar);
+
+var containerEl = document.querySelector('#main-content');
+var mixer = mixitup(containerEl);
